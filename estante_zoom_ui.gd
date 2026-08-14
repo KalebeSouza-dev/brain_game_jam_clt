810209724b4fda039db4acc1f78c688ecb7a8d16
KalebeSouza-dev <kalebesouza.cc@gmail.com>
2026-08-14 17:52:31 -0300

    feat: cria um simples hotbar

--- estante_zoom_ui.gd
+++ estante_zoom_ui.gd
@@ -9,3 +9,10 @@ func _on_button_pressed() -> void:
 	
 	# 2. Destrói esta tela, fechando a interface
 	queue_free()
+
+
+func _on_peca_1_pressed() -> void:
+	Inventario.adicionar_item("Peça 1 - filtro oleo")
+	
+	$ColorRect/Peca1.queue_free()
+	#pass # Replace with function body.
--- scripts/inventario.gd
+++ scripts/inventario.gd
@@ -1,7 +1,10 @@
 extends Node
 
 var itens: Array = []
+signal inventario_atualizado(lista_de_itens)
 
 func adicionar_item(nome_da_peca: String) -> void:
 	itens.append(nome_da_peca)
 	print("Item adicionado! Seu inventário agora tem: ", itens)
+	
+	inventario_atualizado.emit(itens)
